747fc56e7a852ac949ac3c886d1a034dbd8219a1
Khalid Moharrum <khalid.moharram@gmail.com>
2018-05-10 20:34:04 +0200

    Init Commit

---
+++ src/UtilitiesServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Moharrum\Utilities;
+
+use Illuminate\Support\ServiceProvider;
+
+class UtilitiesServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
